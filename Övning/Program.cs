90221a4c1118b6c9d48a17aedccf63311023a44d
elli08003 <elli08003@E-PW04SM2Y>
2025-09-17 13:23:08 +0200

    Add project files.

---
+++ Övning/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Övning2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många extra dagar ska du hyra blien.");
+            int d = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många km ska du tänker du köra.");
+            int km = int.Parse(Console.ReadLine());
+            int kostnad = (km + (d * 500) + 300);
+            Console.WriteLine("Din totala kostnad är " + kostnad + "kr");
+        }
+    }
+}
